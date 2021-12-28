6eb09f3f8db7051147826cc40c8b860ee5e1e55f
PMudunu1 <96665805+PMudunu1@users.noreply.github.com>
2021-12-28 13:55:51 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,47 +1,15 @@
-/*const http = require('http');
-const express = require('express');
-//var cors = require('cors');
-const booksRouter = require('./routes/books');
 
-
-const app = express();
-app.use(express.json());
-//app.use(cors({origin: 'http://localhost:80'}));
-
-app.use('/books', booksRouter);
-
-
-app.use('/', function(req, res) {
-    res.send('Server is Up');
-});
-
-const server = http.createServer(app);
-//const port = 80;
-
-var port = process.env.PORT || 3000;
-app.listen(port, "0.0.0.0", function() {
-console.log("Listening on Port 3000");
-});
-
-server.listen(port);
-console.debug('Server port ' + port);
-*/
-
-//server.js
 const http = require('http');
 const express = require('express')
-//const path = require('path')
+
 
 const app = express()
 app.use(express.json());
 const booksRouter = require('./routes/books');
-app.use('/books', booksRouter);
-//app.use(express.static(path.resolve(__dirname, 'public')));
-// and all the other stuff
+app.use('/api/books', booksRouter);
+
 module.exports = app
 
-//app.js
-//const app = require('./server');
 const port = process.env.PORT || 3000;
 app.listen(port, '0.0.0.0', () => {
     console.log('Server is running s on port: ' + port)
